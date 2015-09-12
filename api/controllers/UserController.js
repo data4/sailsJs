91922b457bb8c35b052e2891c481331087e576a8
Alecs <alecs.garza@akora.mx>
2015-09-12 11:23:48 -0500

    full

---
+++ api/controllers/UserController.js
@@ -0,0 +1,112 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+   new: function (req, res){
+    sails.log('Session --------------', req.session);
+    res.locals.flash = _.clone(req.session.flash);
+    req.session.flash = {};
+    res.view();
+
+   },
+
+   create: function(req, res, next) {
+
+    var userObj = {
+      name: req.param('name'),
+      title: req.param('title'),
+      email: req.param('email'),
+      password: req.param('password'),
+      confirmation: req.param('confirmation')
+    }
+
+    // Create a User with the params sent from
+    // the sign-up form --> new.ejs
+    User.create(userObj, function userCreated(err, user) {
+
+      // // If there's an error
+      // if (err) return next(err);
+
+      if (err) {
+        console.log(err);
+
+        //req.session  Variables disponibles mientras el usuario tiene el browser tab abierto
+        req.session.flash = {
+          err: err
+        }
+
+        // If error redirect back to sign-up page
+        return res.redirect('/user/new');
+      }
+
+      res.json(user);
+      req.session.flash = {};
+
+    });
+  },
+
+
+  // render the profile view (e.g. /views/show.ejs)
+  show: function(req, res, next) {
+    User.findOne(req.param('id'), function foundUser(err, user) {
+      if (err) return next(err);
+      if (!user) return next();
+      res.view({
+        user: user
+      });
+    });
+  },
+
+  // render the edit view (e.g. /views/edit.ejs)
+  edit: function(req, res, next) {
+
+    // Find the user from the id passed in via params
+    User.findOne(req.param('id'), function foundUser(err, user) {
+      if (err) return next(err);
+      if (!user) return next('User doesn\'t exist.');
+
+      res.view({
+        user: user
+      });
+    });
+  },
+
+  // process the info from edit view
+  update: function(req, res, next) {
+
+
+    var userObj = {
+        name: req.param('name'),
+        title: req.param('title'),
+        email: req.param('email')
+      }
+
+    User.update(req.param('id'), userObj, function userUpdated(err) {
+      if (err) {
+        return res.redirect('/user/edit/' + req.param('id'));
+      }
+
+      res.redirect('/user/show/' + req.param('id'));
+    });
+  },
+
+  index: function(req, res, next) {
+
+    // Get an array of all users in the User collection(e.g. table)
+    User.find(function foundUsers(err, users) {
+      if (err) return next(err);
+      // pass the array down to the /views/index.ejs page
+      res.view({
+        users: users
+      });
+    });
+  },
+
+
+};
+
